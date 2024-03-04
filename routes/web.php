c941aae64684c236e97548877e4d175107e3fb74
jgnunes1 <jgnunes@gmail.com>
2024-03-04 11:46:44 -0300

    novo index

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/escola-santo-amaro', 'EscolaController@apresentacao')->name('escola.apresentacao');
-
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });

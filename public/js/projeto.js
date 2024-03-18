129f639c2249c1448635e5cf920ad35137b9a747
jgnunes1 <jgnunes@gmail.com>
2024-03-18 13:56:49 -0300

    blocUI

--- public/js/projeto.js
+++ public/js/projeto.js
@@ -1,7 +1,24 @@
-function deleteRegistrPaginacao(rotaUrl, idDoRegistro){
-
-	if (confirm('Deseja excluir mesmo?')) {
-
-	}
-
+function deleteRegistrPaginacao(rotaUrl, idDoRegistro) {
+    if (confirm('Deseja excluir mesmo?')) {
+        $.ajax({
+            url: rotaUrl,
+            method: 'DELETE',
+            headers: {},
+            data: {
+                id: idDoRegistro,
+            },
+            beforeSend: function() {
+                $.blockUI({
+                    message: 'Carregando...',
+                    timeout: 2000,
+                });
+            },
+        }).done(function(data) {
+            $.unblockUI();
+            console.log(data);
+        }).fail(function(data) {
+            $.unblockUI();
+            alert('Não foi possível excluir o registro');
+        });
+    }
 }
